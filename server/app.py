6bcb1e9f8c74e82bad4690c45be86b7f8038baa1
synicalToo <weijielim@yahoo.com>
2024-05-26 18:58:37 +0800

    removed testing routes

--- server/app.py
+++ server/app.py
@@ -35,9 +35,9 @@ def upload_image():
         image_data_binary = base64.b64decode(image_data_base64)
 
         image_counter += 1
-        
+
         filename = f"image_{image_counter}.jpg"
-        
+
         with open(os.path.join(UPLOAD_FOLDER, filename), "wb") as f:
             print(f"saving image {filename=}")
             f.write(image_data_binary)
@@ -55,18 +55,6 @@ def upload_image():
             return jsonify({"message: ": "No fire detected."})
     except Exception as e:
         return jsonify({"error": str(e)})
-
-
-@app.route("/test_post", methods=["POST"])
-def test_post():
-    print("testing POST method")
-    return {"message: ": "Test successful!"}
-
-
-@app.route("/test_get", methods=["GET"])
-def test_get():
-    print("testing GET method")
-    return {"message: ": "Got test message!"}
 
 
 if __name__ == "__main__":
