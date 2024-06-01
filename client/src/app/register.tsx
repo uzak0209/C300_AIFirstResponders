609159afdb66be72cde3f39ac0896cddf43828f6
synicalToo <weijielim@yahoo.com>
2024-06-01 13:49:52 +0800

    added register screen model

--- client/src/app/register.tsx
+++ client/src/app/register.tsx
@@ -1,9 +1,28 @@
 import { Button, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { FIREBASE_AUTH } from '../../firebaseConfig';
+import { router } from 'expo-router';
 
 export default function register() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+  const auth = FIREBASE_AUTH;
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(response);
+      alert("Registration success");
+      router.back()
+    } catch (error: any) {
+      alert("Registration failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -12,7 +31,7 @@ export default function register() {
         <TextInput style={styles.input} value={email} placeholder='example@email.com' onChangeText={(text) => setEmail(text)}></TextInput>
         <TextInput style={styles.input} value={password} placeholder='Password' secureTextEntry={true} onChangeText={(text) => setPassword(text)}></TextInput>
 
-        <Button title='Register account'></Button>
+        <Button title='Register account' onPress={signUp}></Button>
       </KeyboardAvoidingView>
     </View>
   )
